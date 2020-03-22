6b5f8251e5279e2900f7f0d778f24f4b783dc2d8
Winarto Sinatra <56412330+winartosinatra@users.noreply.github.com>
2020-03-22 08:46:21 +0700

    Pertemuan Pertama

---
+++ index.php
@@ -0,0 +1,12 @@
+<html> 
+<head> 
+<title>
+Index Harga Handphone
+</title> 
+</head> 
+<body> 
+<h3>
+Selamat Datang di Webstite Harga HP
+</h3> 
+</body> 
+</html> 
---
+++ list_phone.php
@@ -0,0 +1,11 @@
+<?php 
+include('connection.php'); 
+$query = 'select id, phone_name nama, price harga from tbl_phone'; 
+$result = mysql_query($query) or die(mysql_error()); 
+$data = array(); 
+while($row = mysql_fetch_object($result))
+{ 
+    $data['handphone'][]= $row; } 
+    echo json_encode($data); 
+    
+    ?> 
